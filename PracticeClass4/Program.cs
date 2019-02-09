337fa887e767a224acacb1ec336094f6c769b730
dieguits <dapohp@gmail.com>
2019-02-09 14:28:28 -0500

    Program to calculate the average of the mark.

--- PracticeClass4/Program.cs
+++ PracticeClass4/Program.cs
@@ -10,6 +10,32 @@ namespace PracticeClass4
     {
         static void Main(string[] args)
         {
+            int mark;
+            Console.WriteLine("Enter the mark: ");
+            mark = Convert.ToInt32(Console.ReadLine());
+
+            if (mark <= 50)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (mark >= 90)
+            {
+                Console.WriteLine("Excellent");
+            }
+            else if (mark > 80 && mark < 90)
+            {
+                Console.WriteLine("Very Good");
+            }
+            else if (mark > 70 && mark < 80)
+            {
+                Console.WriteLine("Good");
+            }
+            else
+            {
+                Console.WriteLine("Average");
+            }
+
+            Console.ReadKey();
         }
     }
 }
