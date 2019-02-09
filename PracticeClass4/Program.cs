1827c2903988cc3c0e3c1ee5337abef40912348b
dieguits <dapohp@gmail.com>
2019-02-09 14:39:27 -0500

    Setup better the marks and conditios

--- PracticeClass4/Program.cs
+++ PracticeClass4/Program.cs
@@ -15,25 +15,15 @@ namespace PracticeClass4
             mark = Convert.ToInt32(Console.ReadLine());
 
             if (mark <= 50)
-            {
                 Console.WriteLine("Fail");
-            }
             else if (mark >= 90)
-            {
                 Console.WriteLine("Excellent");
-            }
-            else if (mark > 80 && mark < 90)
-            {
+            else if (mark >= 80 && mark < 90)
                 Console.WriteLine("Very Good");
-            }
-            else if (mark > 70 && mark < 80)
-            {
+            else if (mark >= 70 && mark < 80)
                 Console.WriteLine("Good");
-            }
             else
-            {
                 Console.WriteLine("Average");
-            }
 
             Console.ReadKey();
         }
