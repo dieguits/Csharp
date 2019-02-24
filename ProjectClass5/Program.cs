adf14207aea32df0a7345382e10ca776ec0ace74
dieguits <dapohp@gmail.com>
2019-02-24 17:21:33 -0500

    The same exercise with Do while

--- ProjectClass5/Program.cs
+++ ProjectClass5/Program.cs
@@ -13,7 +13,7 @@ namespace ProjectClass5
             int sum = 5;
             int i = 0;
 
-            //Infinitive loop
+            //for loop
             //5 + 10 + 15 + 20 + 25
             for (i = 10; i <= 25; i+=5)
             {
@@ -26,6 +26,7 @@ namespace ProjectClass5
             int sum2 = 5;
             int i2 = 10;
 
+            //While loop
             while (i2 <= 25)
             {
                 sum2 += i2;
@@ -34,6 +35,19 @@ namespace ProjectClass5
 
             Console.WriteLine("Sum: " + sum2);
             Console.WriteLine("i: " + i2);
+
+            //Do while
+            int sum3 = 5;
+            int i3 = 10;
+            do
+            {
+                sum3 += i3;
+                i3 += 5;
+            } while (i3 <= 25);
+
+            Console.WriteLine("Sum: " + sum3);
+            Console.WriteLine("i: " + i3);
+
             Console.Read();
         }
     }
