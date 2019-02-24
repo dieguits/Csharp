678e43093eeb5105dc3c6379eb2cef4b28eaf780
dieguits <dapohp@gmail.com>
2019-02-24 17:37:51 -0500

    Exercise number 2 with for loop

---
+++ ProjectClass5.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectClass5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //loop 200 + 190 + 180 + ... + 160 when iteration = 10
+            int i = 0;
+            int sum = 200;
+            int iter = 0;
+
+            for (i = 190; i >= 100; i-=10)
+            {
+                sum += i;
+                iter++;
+            }
+
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("i: " + i);
+            Console.WriteLine("Iterations: " + iter);
+
+            Console.ReadKey();
+        }
+    }
+}
