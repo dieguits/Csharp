9a886c0908e7be38b1bd5d6e0221c5d9c969095c
dieguits <dapohp@gmail.com>
2019-03-17 09:15:15 -0500

    Examples of methods and convert functions

---
+++ Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12, y = 5;
+            int n = Calculate(x, y:y, name:"Diego");
+
+            Console.WriteLine("The result: " + n);
+            
+
+            Double.Parse("12.6");
+            float.Parse("12.7");
+            decimal.Parse("12.7");
+
+            Convert.ToDecimal(x);
+            float.TryParse("12.7", out float n1);
+            n1 = 12.7f;
+            Console.WriteLine(n1);
+            Console.ReadKey();
+        }
+
+        private static int Calculate(int x = 0, int y= 0, string name= "NoName")
+        {
+            int n = 0;
+            n = x + y;
+            Console.WriteLine("Hello " + name);
+
+            return n;
+        }
+    }
+}
