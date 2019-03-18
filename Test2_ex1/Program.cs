2730c637d00f9b68f02a2d713914cb81a4adc89b
dieguits <dapohp@gmail.com>
2019-03-18 18:29:54 -0500

    Excercise one test two

---
+++ Test2_ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 + x + x^2 / 2 + x^3/3
+
+            int n = 4;
+            int x = 2;
+            double result = 1 + x;
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = result + Math.Pow(x, i) / i;
+            }
+
+            Console.WriteLine("Result is: " + result);
+            Console.ReadKey();
+        }
+    }
+}
